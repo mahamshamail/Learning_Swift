07ae86040e81916f3c55e67c2481e018970213da
Maham Shamail <50436238+mahamshamail@users.noreply.github.com>
2020-10-27 23:40:54 -0400

    Create isPerfect.swift
    This is a simple swift function that checks if a number is perfect or not

---
+++ isPerfect.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func isPerfect (num: Int) -> Bool{
+    guard num >= 2 else {
+        return false
+
+    }
+    var i = 1
+    var sum = 0
+    while(i < num){
+        if num % i == 0{
+            print("factor: \(i)")
+            sum = sum + i
+        }
+        i += 1
+    }
+    if(sum == num){
+        return true
+    }
+    else{
+        return false
+    }
+}
+print(isPerfect(num: 0))
